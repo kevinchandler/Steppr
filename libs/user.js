219d891a487f2558f303ce03a46ca8adacacd790
Kevin Chandler <im.kevin@me.com>
2014-03-29 21:53:20 -0700

    callback doc

--- libs/user.js
+++ libs/user.js
@@ -137,7 +137,7 @@ module.exports = {
 					callback(err)
 				}
 				if (doc.username) {
-					callback( null, true );
+					callback( null, doc );
 				}
 				else {
 					callback( null, false );
